c2ee729927ad3bb384d91ec5ce4d14051d803c37
Andre Peres <andreperes.dev@gmail.com>
2022-10-03 21:25:32 -0300

    :tada: add default calculator operaitons

---
+++ src/components/Input/styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAFFAA;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #AAFFAA;
+        border: 0;
+        display: flex;
+        align-items: flex-end;
+        text-align: right;
+        padding: 0 10px;
+        font-size: 24px;
+        font-family: 'Roboto';
+    }
+`
